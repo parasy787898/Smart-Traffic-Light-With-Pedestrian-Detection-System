ba03f2b556bb7ab36a740ab8bf5d85b0d54b0b71
Paras Yadav <51355144+parasy787898@users.noreply.github.com>
2022-06-07 14:38:20 +0530

    Add files via upload

---
+++ yolov5/det.py
@@ -0,0 +1,32 @@
+from sqlite3 import Time
+from turtle import st
+import pygame
+import random
+from os import path
+import time
+import shelve
+from os import path
+from PIL import Image, ImageTk
+
+
+
+file=path.join(path.dirname(__file__),"lib","test")
+
+v7=[[5,1], path.join(path.dirname(__file__),"lib","test","v7.jpg") ]
+v6=[[5,2], path.join(path.dirname(__file__),"lib","test","v6.jpg") ]
+v5=[[10,0], path.join(path.dirname(__file__),"lib","test","v5.jpg") ]
+v4=[[13,2], path.join(path.dirname(__file__),"lib","test","v4.jpg") ]
+v3=[[15,1], path.join(path.dirname(__file__),"lib","test","v3.jpg") ]
+v2=[[8,2], path.join(path.dirname(__file__),"lib","test","v2.jpg") ]
+v1=[[6,1], path.join(path.dirname(__file__),"lib","test","v1.jpg") ]
+
+l3=[[7,2], path.join(path.dirname(__file__),"lib","test","l3.png") ]
+l2=[[4,8], path.join(path.dirname(__file__),"lib","test","l2.png") ]
+l1=[[13,0], path.join(path.dirname(__file__),"lib","test","l1.png") ]
+
+
+i1=[3,  path.join(path.dirname(__file__),"lib","test","i1.jpg") ]
+i2= [2, path.join(path.dirname(__file__),"lib","test","i2.jpg") ]
+i3=[7 , path.join(path.dirname(__file__),"lib","test","i3.jpg") ]
+
+
